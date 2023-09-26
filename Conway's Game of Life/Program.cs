051a23c6ee1e882a31c875d586fa1430865869d8
rtinase <63422724+rtinase@users.noreply.github.com>
2023-09-26 11:14:39 +0200

    Update Program.cs
    Updated the main program, there is still problem with calculating of neighbors

--- Conway's Game of Life/Program.cs
+++ Conway's Game of Life/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Data;
-using System.Drawing;
+using System;
 
 namespace Program{
     class Program{
@@ -9,12 +7,19 @@ namespace Program{
             int amtColumns = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many rows do you want?");
             int amtRows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many steps do you want?");
+            int amtSteps = Convert.ToInt32(Console.ReadLine());
+
 
             Game myGame = new Game();
-            for(int i = 0; i < 10; i++){
-                bool[,] fields = myGame.make2DArray(amtRows, amtColumns);
-                myGame.makeGrid(fields);
-                Thread.Sleep(1000);
+            int doneSteps = 0;
+
+            bool[,] fields = myGame.make2DArray(amtRows, amtColumns); // create a new random filling of the grid
+            for(int i = 0; i < amtSteps; i++){
+                myGame.makeGrid(fields); // drawing the grid 
+                fields = myGame.computingNextGeneration(fields); // reload of the grid
+                Console.WriteLine("\n\n STEP: " + doneSteps++);
+                Thread.Sleep(500);
                 Console.Clear();
             }
             
@@ -45,8 +50,8 @@ namespace Program{
         public void makeGrid(bool[,] inputArray){ // draws a grid 
             for(int i = 0; i < inputArray.GetLength(0); i++){ 
                 for(int j = 0; j < inputArray.GetLength(1); j++){  
-                    if (inputArray[i,j] == true ) Console.Write("@"); // what sign will be displayd on the place alive cell
-                    else Console.Write("_");    // what sign will be displayd on the place dead cell
+                    if (inputArray[i,j] == true ) Console.Write("0"); // what sign will be displayd on the place alive cell
+                    else Console.Write(".");    // what sign will be displayd on the place dead cell
                 }
                 Console.WriteLine();
             }
@@ -55,16 +60,22 @@ namespace Program{
         private int countLiveNeighbors(bool[,] inputArray, int x, int y){ // counting alive neighbors around one cell
             int sum = 0;
              
-            for(int i = -1; i < 2; i++){
-               for(int k = -1; k < 2; k++){
-
-                if(inputArray[ (x + i + rowsCou) % rowsCou, (y + k +colsCou) % colsCou]) sum++; // checks cells around and adds 1 they are true
+            for(int i = -1; i <= 1; i++){
+               for(int k = -1; k <= 1; k++){
+                try{
+                    if(inputArray[x+i,y+k] == true) sum++;  // checks cells around and adds 1 they are true
+                }
+                catch{
+                    continue; // if the cell is on boundry, doesn't check it
+                }
+                
+                // if(inputArray[ (x + i + rowsCou) % rowsCou, (y + k +colsCou) % colsCou]) sum++;
                }
             }
             return sum--; // return amount of neigbors and minus to not cound itself
         }
 
-        public int computingNextGeneration(bool[,] inputArray){ // next generation
+        public bool[,] computingNextGeneration(bool[,] inputArray){ // next generation
             bool[,] nextGeneration = new bool[inputArray.GetLength(0), inputArray.GetLength(1)]; // the next move array
 
             for(int i = 0; i < inputArray.GetLength(0); i++){
@@ -77,11 +88,14 @@ namespace Program{
                     int neighbors = this.countLiveNeighbors(inputArray, i, k); // checking amount of cells around that are alive
                     bool isAlive = inputArray[i,k]; // checking if this cell alive
 
-                    if( !isAlive && neighbors == 3){
-                        nextGeneration[i,k] = true;
-                    }
-                    else if(isAlive && (neighbors < 2 || neighbors > 3)){
+                    if(isAlive && neighbors < 2){ // cell dies due to loniless or overpopulation
                         nextGeneration[i,k] = false;
+                    }
+                    else if(isAlive && neighbors > 3){
+
+                    }
+                    else if( !isAlive && neighbors == 3){
+                        nextGeneration[i,k] = true;
                     }
                     else{
                         nextGeneration[i,k] = inputArray[i,k];
@@ -89,7 +103,7 @@ namespace Program{
                     
                 }
             }
-            return 0;
+            return nextGeneration;
         }
     }
 }
