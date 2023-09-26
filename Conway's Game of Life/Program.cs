6a456c80d7f92dafe0cfd841a9e8fdf61fcad75a
rtinase <63422724+rtinase@users.noreply.github.com>
2023-09-26 12:38:39 +0200

    Update Program.cs
    Full working version, however there is no verhältnis

--- Conway's Game of Life/Program.cs
+++ Conway's Game of Life/Program.cs
@@ -12,15 +12,15 @@ namespace Program{
 
 
             Game myGame = new Game();
-            int doneSteps = 0;
+            int doneSteps = 1;
 
             bool[,] fields = myGame.make2DArray(amtRows, amtColumns); // create a new random filling of the grid
             for(int i = 0; i < amtSteps; i++){
+                if(i != amtSteps) Console.Clear(); 
                 myGame.makeGrid(fields); // drawing the grid 
                 fields = myGame.computingNextGeneration(fields); // reload of the grid
                 Console.WriteLine("\n\n STEP: " + doneSteps++);
-                Thread.Sleep(500);
-                Console.Clear();
+                Thread.Sleep(500);               
             }
             
         
@@ -63,7 +63,7 @@ namespace Program{
             for(int i = -1; i <= 1; i++){
                for(int k = -1; k <= 1; k++){
                 try{
-                    if(inputArray[x+i,y+k] == true) sum++;  // checks cells around and adds 1 they are true
+                    if(inputArray[x+i,y+k] == true) sum++;  // checks cells around and adds if 1 they are true
                 }
                 catch{
                     continue; // if the cell is on boundry, doesn't check it
@@ -72,7 +72,8 @@ namespace Program{
                 // if(inputArray[ (x + i + rowsCou) % rowsCou, (y + k +colsCou) % colsCou]) sum++;
                }
             }
-            return sum--; // return amount of neigbors and minus to not cound itself
+            sum--; // Subtract 1 to exclude the cell itself as a neighbor.
+            return sum; // return amount of neigbors and minus to not count itself
         }
 
         public bool[,] computingNextGeneration(bool[,] inputArray){ // next generation
@@ -80,10 +81,6 @@ namespace Program{
 
             for(int i = 0; i < inputArray.GetLength(0); i++){
                 for(int k = 0; k < inputArray.GetLength(1); k++){
-
-                    // if(i == 0 || i == inputArray.GetLength(0)-1 || k == 0 || k == inputArray.GetLength(1)-1){// Checking collision of the edjes
-
-                    // } 
                     
                     int neighbors = this.countLiveNeighbors(inputArray, i, k); // checking amount of cells around that are alive
                     bool isAlive = inputArray[i,k]; // checking if this cell alive
@@ -92,7 +89,7 @@ namespace Program{
                         nextGeneration[i,k] = false;
                     }
                     else if(isAlive && neighbors > 3){
-
+                        nextGeneration[i, k] = false; // Cell dies due to overpopulation
                     }
                     else if( !isAlive && neighbors == 3){
                         nextGeneration[i,k] = true;
